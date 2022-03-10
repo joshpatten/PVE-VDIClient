8ff500257986c6b9eba4f2cb48ab06d72bbec7f9
completion <completion>
2022-03-09 15:31:52 -0900

    fill user from config

--- vdiclient.py
+++ vdiclient.py
@@ -132,7 +132,7 @@ def setmainlayout():
 		layout.append([sg.Image(G.imagefile), sg.Text(G.title, size =(18, 1), justification='c', font=["Helvetica", 18])])
 	else:
 		layout.append([sg.Text(G.title, size =(30, 1), justification='c', font=["Helvetica", 18])])
-	layout.append([sg.Text("Username", size =(12, 1), font=["Helvetica", 12]), sg.InputText(key='-username-', font=["Helvetica", 12])])
+	layout.append([sg.Text("Username", size =(12, 1), font=["Helvetica", 12]), sg.InputText(default_text=G.user,key='-username-', font=["Helvetica", 12])])
 	layout.append([sg.Text("Password", size =(12, 1),font=["Helvetica", 12]), sg.InputText(key='-password-', password_char='*', font=["Helvetica", 12])])
 	
 	if G.totp:
