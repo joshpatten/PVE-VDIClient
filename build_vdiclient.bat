e9a653ae984759c0207277bd65a5f485081e1115
jpattWPC <jpatten@westparkcom.net>
2024-01-15 10:20:58 -0600

    Update build_vdiclient.bat

--- build_vdiclient.bat
+++ build_vdiclient.bat
@@ -1,24 +1,24 @@
 @echo off
-pyinstaller --noconsole --noconfirm --hidden-import proxmoxer.backends --hidden-import proxmoxer.backends.https --hidden-import proxmoxer.backends.https.AuthenticationError --hidden-import proxmoxer.core --hidden-import proxmoxer.core.ResourceException --hidden-import subprocess.TimeoutExpired --hidden-import subprocess.CalledProcessError --hidden-import requests.exceptions --hidden-import requests.exceptions.ReadTimeout --hidden-import requests.exceptions.ConnectTimeout --hidden-import requests.exceptions.ConnectionError -i vdiicon.ico vdiclient.py
+pyinstaller --noconsole --noconfirm --hidden-import proxmoxer.backends --hidden-import proxmoxer.backends.https --hidden-import proxmoxer.backends.https.AuthenticationError --hidden-import proxmoxer.core --hidden-import proxmoxer.core.ResourceException --hidden-import subprocess.TimeoutExpired --hidden-import subprocess.CalledProcessError --hidden-import requests.exceptions --hidden-import requests.exceptions.ReadTimeout --hidden-import requests.exceptions.ConnectTimeout --hidden-import requests.exceptions.ConnectionError --noupx -i vdiicon.ico vdiclient.py
 copy vdiclient.png dist\vdiclient
 copy vdiicon.ico dist\vdiclient
-del dist\vdiclient\opengl32sw.dll
-del dist\vdiclient\libGLESv2.dll
-del dist\vdiclient\d3dcompiler_47.dll
-del dist\vdiclient\Qt5Pdf.dll
-del dist\vdiclient\Qt5VirtualKeyboard.dll
-del dist\vdiclient\Qt5WebSockets.dll
-del dist\vdiclient\Qt5Quick.dll
-del dist\vdiclient\PySide2\plugins\imageformats\qgif.dll
-del dist\vdiclient\PySide2\plugins\imageformats\qjpeg.dll
-del dist\vdiclient\PySide2\plugins\imageformats\qpdf.dll
-del dist\vdiclient\PySide2\plugins\imageformats\qsvg.dll
-del dist\vdiclient\PySide2\plugins\imageformats\qtga.dll
-del dist\vdiclient\PySide2\plugins\imageformats\qtiff.dll
-del dist\vdiclient\PySide2\plugins\imageformats\qwbmp.dll
-del dist\vdiclient\PySide2\plugins\imageformats\qwebp.dll
-del dist\vdiclient\PySide2\plugins\platforminputcontexts\qtvirtualkeyboardplugin.dll
-del /Q dist\vdiclient\PySide2\translations\*
+REM del dist\vdiclient\opengl32sw.dll
+REM del dist\vdiclient\libGLESv2.dll
+REM del dist\vdiclient\d3dcompiler_47.dll
+REM del dist\vdiclient\Qt5Pdf.dll
+REM del dist\vdiclient\Qt5VirtualKeyboard.dll
+REM del dist\vdiclient\Qt5WebSockets.dll
+REM del dist\vdiclient\Qt5Quick.dll
+REM del dist\vdiclient\PySide2\plugins\imageformats\qgif.dll
+REM del dist\vdiclient\PySide2\plugins\imageformats\qjpeg.dll
+REM del dist\vdiclient\PySide2\plugins\imageformats\qpdf.dll
+REM del dist\vdiclient\PySide2\plugins\imageformats\qsvg.dll
+REM del dist\vdiclient\PySide2\plugins\imageformats\qtga.dll
+REM del dist\vdiclient\PySide2\plugins\imageformats\qtiff.dll
+REM del dist\vdiclient\PySide2\plugins\imageformats\qwbmp.dll
+REM del dist\vdiclient\PySide2\plugins\imageformats\qwebp.dll
+REM del dist\vdiclient\PySide2\plugins\platforminputcontexts\qtvirtualkeyboardplugin.dll
+REM del /Q dist\vdiclient\PySide2\translations\*
 cd dist
 python createmsi.py vdiclient.json
 cd ..
